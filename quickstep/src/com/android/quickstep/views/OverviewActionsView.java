88e3a28f270105f1b85db5f40688c6885a832400
AmeChanRain <downloadbot007@gmail.com>
2023-04-25 09:27:36 +0000

    MikuHome: Show toast after shake
    * Configurable by overlay.

--- quickstep/src/com/android/quickstep/views/OverviewActionsView.java
+++ quickstep/src/com/android/quickstep/views/OverviewActionsView.java
@@ -24,6 +24,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import androidx.annotation.IntDef;
 import androidx.annotation.Nullable;
@@ -36,6 +37,7 @@ import com.android.launcher3.util.DisplayController;
 import com.android.launcher3.util.MultiPropertyFactory.MultiProperty;
 import com.android.launcher3.util.MultiValueAlpha;
 import com.android.launcher3.util.NavigationMode;
+import com.android.launcher3.util.RandomResUtils;
 import com.android.launcher3.util.ShakeUtils;
 import com.android.quickstep.TaskOverlayFactory.OverlayUICallbacks;
 import com.android.quickstep.util.LayoutUtils;
@@ -167,6 +169,11 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
     public void onShake(double speed) {
 	    if (mCallbacks != null && findViewById(R.id.action_screenshot).getVisibility() == VISIBLE) {
 	        mCallbacks.onClearAllTasksRequested();
+            Toast.makeText(mContext, RandomResUtils.getRandomString(
+                mContext.getResources(),
+                R.array.random_msg_after_shake,
+                mContext.getResources().getInteger(R.integer.random_msg_max)),
+                Toast.LENGTH_SHORT).show();
 	        setCallbacks(null); // Clear the listener after shake
 	    }
     }
